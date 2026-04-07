2cac2731a281ecc606ca1583161e3e7f80f71ae6
Aaron Roper <aroper8@hotmail.com>
2026-04-07 10:59:55 +1000

    Initial Commit

---
+++ Real O Who/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Real O Who
+//
+//  Created by Aaron Roper on 7/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Real O Who/Real_O_WhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_O_WhoApp.swift
+//  Real O Who
+//
+//  Created by Aaron Roper on 7/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Real_O_WhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
